929acb5985fb0a4ac0a5af5420681f2c2ea0d698
caleb316x <hyperactive789@gmail.com>
2019-09-24 17:40:04 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         BigToken Script
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://my.bigtoken.com/*
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// @run-at       document-start
+// @require      http://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+
+(function() {
+ alert("test script");
+})();
